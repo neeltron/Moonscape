98fdab941a943c9b92d03e67a214e474b69a56e3
Neel Adwani <nprosofficial@gmail.com>
2021-09-19 08:55:25 +0000

    Added Replit DB to store sleep timestamp

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 from flask import Flask, request
+from replit import db
+import datetime
 app = Flask('app')
 
 @app.route('/')
 def intro():
   return 'Moonscape API'
 
+data = []
+
+timestamp = datetime.datetime.now()
+print(timestamp)
+
 @app.route('/input', methods = ['GET'])
 def input():
   if request.method == 'GET':
     sleep = request.args.get('sleep')
+    timestamp = datetime.datetime.now()
+    db[timestamp] = sleep
+    print(timestamp)
     print(sleep)
+
   return sleep
 
 app.run(host='0.0.0.0', port=8080)
